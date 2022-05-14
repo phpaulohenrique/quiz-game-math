60366644bf1e3795228a84c8237136677a549067
Paulo Henrique <paulo.henrique1080p@gmail.com>
2022-05-14 20:38:38 -0300

    1º: improvements in logic. 2º: added restart button. 3º: added icon correct or incorrect in the alternative button clicked. 4º: improvements in CSS

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,8 @@
-// let titulo = document.querySelector("h1");
-
-// let instrucoes = document.querySelector("#instrucoes");
-// let aviso = document.querySelector("#aviso");
-//let respostaEsta = document.querySelector('#respostaEsta')
-let pontos = 0; // pontos para o placar
-let placar = 0; // placar
-
-// PERGUNTA
-let numQuestion = document.querySelector("#section__question__num-question");
-
-let question = document.querySelector("#section__question__text-question");
-
-const btnNext = document.querySelector('.btn-next-question')
-
-const btnStartQuiz = document.querySelector(".btn-start-quiz");
-
-// const allLiAnswer = document.querySelectorAll(".answers");
-
-
-btnStartQuiz.addEventListener("click", () => {
-    
-    startQuiz();
-});
-
-btnNext.addEventListener("click", () => {
-    nextQuestion();
-})
-
-// ALTERNATIVAS
-let a = document.querySelector("#a");
-let b = document.querySelector("#b");
-let c = document.querySelector("#c");
-
-// article com a class questoes
-let articleQuestoes = document.querySelector(".article-questions");
-// ol li com as alternativas
-let alternatives = document.querySelector("#alternatives");
 const questions = [
 
     {
         numQuestion: 1,
-        question: "5+5?",
+        question: "5 + 5?",
         alternatives : {
             alternativeA: "10",
             alternativeB: "15",
@@ -51,7 +13,7 @@ const questions = [
     {
 
         numQuestion: 2,
-        question: "Quantos é 2 + 2?",
+        question: "2 + 2?",
         alternatives : {
 
             alternativeA: "6",
@@ -63,7 +25,7 @@ const questions = [
     {
 
         numQuestion: 3,
-        question: "Quantos é 5 + 20",
+        question: "5 + 20?",
         alternatives : {
             
             alternativeA: "10",
@@ -75,12 +37,61 @@ const questions = [
     },
 ]
 
-// const questions = [question1, question2];
-const totalQuestions = (questions.length);
-console.log('total de questoes', totalQuestions);
 
+let assessment = 0; // pontuação
+let scoreboard = 0; // placar
+
+// QUESTIONS
+const sectionAlternativesQuestion = document.querySelector('.section__alternatives-question');
+
+let numQuestion = document.querySelector("#section__question__num-question");
+let question = document.querySelector("#section__question__text-question");
+let alternatives = document.querySelector("#alternatives");
+
+const btnNext = document.querySelector('.btn-next-question')
+const btnStartQuiz = document.querySelector(".btn-start-quiz");
+const btnRestartQuiz = document.querySelector(".btn-restart-quiz");
+
+let btnAnswerClicked = null;
+
+// ALTERNATIVAS
+let a = document.querySelector("#a");
+let b = document.querySelector("#b");
+let c = document.querySelector("#c");
+
+const CLICK = "click";
+
+const totalQuestions = questions.length;
+
+
+btnStartQuiz.addEventListener(CLICK, () => {
+    startQuiz();
+});
+
+
+btnNext.addEventListener(CLICK, () => {
+    nextQuestion();
+});
+
+
+btnRestartQuiz.addEventListener(CLICK, () => {
+    if(confirm('Deseja reiniciar o Quiz?')){
+        console.log('reiniciou')
+        resetButtonColorAnswerCorretOrIncorret();
+        toggleEnabledAlternativesButtons();
+        startQuiz();
+    }
+});
+
+
+// console.log("total de questoes", totalQuestions);
 
 const startQuiz = () => {
+
+    btnNext.classList.remove('is--disable')
+    sectionAlternativesQuestion.classList.remove('is--disable');
+    btnStartQuiz.classList.add('is--disable')
+    btnRestartQuiz.classList.remove('is--disable')
 
     numQuestion.textContent = `${questions[0].numQuestion} / ${totalQuestions}`;
     question.textContent = questions[0].question;
@@ -89,96 +100,68 @@ const startQuiz = () => {
     b.textContent = questions[0].alternatives.alternativeB;
     c.textContent = questions[0].alternatives.alternativeC;
 
-
-    // a.setAttribute("value", questions[0].alternatives.alternativeA);
-    // b.setAttribute("value", "1B");
-    // c.setAttribute("value", "1C");
-
 }
-// a.setAttribute("value", "1A");
-// a.setAttribute("value", "1A");
-// b.setAttribute("value", "1B");
-// c.setAttribute("value", "1C");
 
-function toggleEnabledAlternativesButtons() {
-    document.querySelectorAll(".alternatives__answer-button").forEach((item) => {
-    item.toggleAttribute("disabled");
+
+const toggleEnabledAlternativesButtons = () => {
+    document.querySelectorAll(".alternatives__answer-button").forEach((button) => {
+    button.toggleAttribute("disabled");
     });
 }
-
-var btnAnswerClicked = null;
 
 
 const checkAnswer = (nQuestion, answer, liClicked) => {
 
     btnAnswerClicked = liClicked;
-    console.log(liClicked)
+    // console.log(liClicked)
+
+    toggleEnabledAlternativesButtons();
+    // console.log(nQuestion.value, answer.textContent);
 
     // console.log(liClicked.id);
-    toggleEnabledAlternativesButtons();
-
-    // const allLiAnswer = document.querySelectorAll(".alternatives__answer-button");
-
-    // document.querySelectorAll(".answers").forEach((item) => {
-    //     item.addEventListener("click", (event) => {
-    //     item.setAttribute("disabled");
-    //     isDisableButtons();
-    // });
-    // });
-
-    // console.log(allLiAnswer)
-
-    // // const b = JSON.parse(allLiAnswer);
-
-    // console.log(b)
-
-    // const test = b.map((item) => item.id == liClicked.id)
-
-    // console.log(test)
-
-    // const a = Array.from(allLiAnswer, li => li.id != liClicked.id)
-    // console.log(a)
-
-    // const LisNotClicked = allLiAnswer.forEach((li) => li.id)
-    
-    // console.log(LisNotClicked)
-
-    console.log(nQuestion.value, answer.textContent);
-
-    // const liClicked = document.querySelector(".btn-next-question").id == liClicked.id;
-
-    // const cart = Array.from(allLiAnswer).filter(button => {
-    //     // button.id === "a";
-    //     console.log(button.id == "a")
-
-    // })
-
-    // console.log(cart)
-    console.log(liClicked.id);
-
     const nCurrentQuestion = nQuestion.value;
     const chosenAnswer = answer.textContent;
 
-    console.log(nCurrentQuestion);
+    // console.log(nCurrentQuestion);
     const correctAnswer = questions[nCurrentQuestion-1].alternatives.correct;
 
     if(chosenAnswer == correctAnswer){
         liClicked.classList.add("is--correct");
         liClicked.setAttribute("clicked", 'true')
-    }
-    else{
-        liClicked.classList.add("is--incorrect");
+
+        createIconCorrectOrIncorrectInAlternative("correct");
     }
 
-    // return btnAnswerClicked;
+    else{
+        liClicked.classList.add("is--incorrect");
+        createIconCorrectOrIncorrectInAlternative('incorrect');
+    }
+}
+
+const createIconCorrectOrIncorrectInAlternative = (correctOrIncorret) =>{
+
+    const icon = document.createElement("img");
+    icon.classList.add(
+        "alternatives__answer-button__icon-correct-or-incorrect"
+    );
+
+    icon.setAttribute("src", `../assets/icon-${correctOrIncorret}.png`);
+    btnAnswerClicked.appendChild(icon);
 
 }
 
-const nextQuestion = () => {
+
+const resetButtonColorAnswerCorretOrIncorret = () =>{
+    btnAnswerClicked.classList.remove("is--correct", "is--incorrect");
+}
+
+const nextQuestion = (event) => {
+
+    // console.log(event)
 
     console.log(btnAnswerClicked);
+    resetButtonColorAnswerCorretOrIncorret();
 
-    btnAnswerClicked.classList.remove('is--correct', 'is--incorrect');
     numCurrentQuestion = document.querySelector('#section__question__num-question').innerText;    
     numCurrentQuestionNumber = parseFloat(numCurrentQuestion);
     console.log(numCurrentQuestionNumber);
@@ -186,19 +169,18 @@ const nextQuestion = () => {
     nextPositionQuestionArray = numCurrentQuestionNumber;
 
     numQuestion.textContent = `${questions[numCurrentQuestionNumber].numQuestion} / ${totalQuestions}`;
+
+    // if  the user clicked direct in btn "next",  without selecting any answer thow this alert below, if that happens the 
     question.textContent = questions[nextPositionQuestionArray].question;
 
     a.textContent = questions[nextPositionQuestionArray].alternatives.alternativeA;
     b.textContent = questions[nextPositionQuestionArray].alternatives.alternativeB;
     c.textContent = questions[nextPositionQuestionArray].alternatives.alternativeC;
 
-
     a.setAttribute("value", nextPositionQuestionArray + 1);
     b.setAttribute("value", nextPositionQuestionArray + 1);
     c.setAttribute("value", nextPositionQuestionArray + 1);
 
     toggleEnabledAlternativesButtons();
-    // b.setAttribute("value", "1B");
-    // c.setAttribute("value", "1C");
 }
 
